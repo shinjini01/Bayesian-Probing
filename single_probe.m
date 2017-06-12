1deb456d94456cd187f41314bd2eae6eaa051067
shinjini01 <tuf56938@temple.edu>
2017-06-11 22:33:40 -0500

    Create single_probe.m

--- single_probe.m
+++ single_probe.m
@@ -3,22 +3,21 @@ function tau = single_probe(prob_matrix, n)
 Q = 1 - prob_matrix;
 delta_U = max(sum(Q,1));
 tau = find(sum(Q, 1) == delta_U, 1 ); %find minimum index with maximum value
-a = 1:n; tau0 = tau;
+indices = find(1:n ~= tau);
 
    
-while(~isempty(a))
-    a = find(1:n ~= tau0);
-    for j = a
-        b = (length(tau0)+1)*sum( prod(Q(:,tau0),2).*Q(:,j));
-        if(b >= delta_U)
-            delta_U = b;
-            tau = sort([tau, j]);
+while(~isempty(indices))
+    w_i = [];
+    for j = 1:length(indices)
+        w_i = [w_i,(length(tau)+j)*sum( prod(Q(:,tau),2).*Q(:,indices[1:j]))];
+        end;
+        
+        if(max(w_i) >= delta_U)
+            delta_U = w_i;
+            tau = sort([tau, 1:find(w_i==max(w_i))]);
         end
-    end
-    if(tau == tau0)
-        break;
     else
-    tau0 = tau;
+    break;
     end
     
 end
