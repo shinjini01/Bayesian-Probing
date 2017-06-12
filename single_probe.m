b8f1689c205113ca08fdaa1e47655fa45f6c1f63
shinjini01 <tuf56938@temple.edu>
2017-06-11 19:31:01 -0500

    Add files via upload

---
+++ single_probe.m
@@ -0,0 +1,29 @@
+%% Function to find single best probes 
+function tau = single_probe(prob_matrix, n)
+Q = 1 - prob_matrix;
+delta_U = max(sum(Q,1));
+tau = find(sum(Q, 1) == delta_U, 1 ); %find minimum index with maximum value
+a = 1:n; tau0 = tau;
+
+   
+while(~isempty(a))
+    a = find(1:n ~= tau0);
+    for j = a
+        b = (length(tau0)+1)*sum( prod(Q(:,tau0),2).*Q(:,j));
+        if(b >= delta_U)
+            delta_U = b;
+            tau = sort([tau, j]);
+        end
+    end
+    if(tau == tau0)
+        break;
+    else
+    tau0 = tau;
+    end
+    
+end
+
+        
+end
+
+
