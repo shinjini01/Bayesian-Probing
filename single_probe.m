4c632aa8a0c2a76b5eb705b996c6c1cca3584d0a
shinjini01 <tuf56938@temple.edu>
2017-06-12 10:37:27 -0500

    Update single_probe.m

--- single_probe.m
+++ single_probe.m
@@ -1,4 +1,5 @@
-function tau = single_probe(prob_matrix, n)
+%% Function to find single best probes 
+function tau = single_probe2(prob_matrix, n)
 Q = 1 - prob_matrix; 
 delta_U = max(sum(Q,1));
 tau = find(sum(Q, 1) == delta_U, 1 ); %find minimum index with maximum value
@@ -6,13 +7,13 @@ indices = setdiff(1:n,tau);
    
 while(~isempty(indices))
     indices = setdiff(1:n,tau);
-    w_i = NaN(1,length(indices));
+    delta_U_i = NaN(1,length(indices));
     for j = 1:length(indices)
-        w_i(j) = (length(tau)+j)*sum( prod(Q(:,tau),2).*prod(Q(:,indices(1:j)),2));
+        delta_U_i(j) = (length(tau)+j)*sum( prod(Q(:,tau),2).*prod(Q(:,indices(1:j)),2));
     end
-    w_i = w_i(~isnan(w_i));
-        if(max(w_i) > delta_U)
-            delta_U = max(w_i); max_index = find(w_i == max(w_i),1);
+    delta_U_i = delta_U_i(~isnan(delta_U_i));
+        if(max(delta_U_i) > delta_U)
+            delta_U = max(delta_U_i); max_index = find(delta_U_i == max(delta_U_i),1);
             tau = unique(sort(horzcat(tau, indices(1:max_index))));
         else
             break
@@ -20,4 +21,6 @@ while(~isempty(indices))
       
 end
 
+        
 end
+
