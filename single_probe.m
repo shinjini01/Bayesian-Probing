2fe840d08a0928524bfc9efcb81cf10b96e78c78
shinjini01 <tuf56938@temple.edu>
2017-06-12 10:31:40 -0500

    Create single_probe.m

--- single_probe.m
+++ single_probe.m
@@ -1,29 +1,23 @@
-%% Function to find single best probes 
 function tau = single_probe(prob_matrix, n)
-Q = 1 - prob_matrix;
+Q = 1 - prob_matrix; 
 delta_U = max(sum(Q,1));
 tau = find(sum(Q, 1) == delta_U, 1 ); %find minimum index with maximum value
-a = 1:n; tau0 = tau;
-
+indices = setdiff(1:n,tau);
    
-while(~isempty(a))
-    a = find(1:n ~= tau0);
-    for j = a
-        b = (length(tau0)+1)*sum( prod(Q(:,tau0),2).*Q(:,j));
-        if(b >= delta_U)
-            delta_U = b;
-            tau = sort([tau, j]);
+while(~isempty(indices))
+    indices = setdiff(1:n,tau);
+    w_i = NaN(1,length(indices));
+    for j = 1:length(indices)
+        w_i(j) = (length(tau)+j)*sum( prod(Q(:,tau),2).*prod(Q(:,indices(1:j)),2));
+    end
+    w_i = w_i(~isnan(w_i));
+        if(max(w_i) > delta_U)
+            delta_U = max(w_i); max_index = find(w_i == max(w_i),1);
+            tau = unique(sort(horzcat(tau, indices(1:max_index))));
+        else
+            break
         end
-    end
-    if(tau == tau0)
-        break;
-    else
-    tau0 = tau;
-    end
-    
+      
 end
 
-        
 end
-
-
