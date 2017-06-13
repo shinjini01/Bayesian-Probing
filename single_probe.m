93fb65d8dc0ea7cf3a37ce4c5e2ec29ceb106ce8
shinjini01 <tuf56938@temple.edu>
2017-06-13 14:59:54 -0500

    Create single_probe.m

--- single_probe.m
+++ single_probe.m
@@ -1,5 +1,4 @@
-%% Function to find single best probes 
-function tau = single_probe2(prob_matrix, n)
+function tau = single_probe(prob_matrix,m, n)
 Q = 1 - prob_matrix; 
 delta_U = max(sum(Q,1));
 tau = find(sum(Q, 1) == delta_U, 1 ); %find minimum index with maximum value
@@ -7,11 +6,17 @@ indices = setdiff(1:n,tau);
    
 while(~isempty(indices))
     indices = setdiff(1:n,tau);
-    delta_U_i = NaN(1,length(indices));
+    delta_U_i = zeros(1,length(indices));
+   
     for j = 1:length(indices)
-        delta_U_i(j) = (length(tau)+j)*sum( prod(Q(:,tau),2).*prod(Q(:,indices(1:j)),2));
+        
+        for i = 1:m
+            indices_i = indices(Q(i,indices(1:j))<1);
+            tau_i = tau(Q(i, tau) <1);
+            delta_U_i(j) = delta_U_i(j) + (length(tau_i)+length(indices_i))*sum( prod(Q(i,tau_i),2)*prod(Q(i,indices_i),2));
+        end
     end
-    delta_U_i = delta_U_i(~isnan(delta_U_i));
+    %delta_U_i = delta_U_i(~isnan(delta_U_i));
         if(max(delta_U_i) > delta_U)
             delta_U = max(delta_U_i); max_index = find(delta_U_i == max(delta_U_i),1);
             tau = unique(sort(horzcat(tau, indices(1:max_index))));
