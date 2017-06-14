a26d8fa2a0b55b4bbc37eb333a1652fb7fac8571
shinjini01 <tuf56938@temple.edu>
2017-06-14 09:30:17 -0500

    Update update_probability.m

--- update_probability.m
+++ update_probability.m
@@ -4,8 +4,8 @@ function P_mat = update_probability(probe_vec, Prob_matrix, result_vec)
 Q = 1 - Prob_matrix;
 tau = find(probe_vec); % Find the indices of the non-zero values in the probe vector
 for i = 1: size(Prob_matrix, 1)
-    w = prod(Q(i,tau));
-
+    w = prod(Q(i,tau)); % Calculate w_i values as in eq 12, section 2.5
+% Update the probability values as in eq. 15, section 2.5
 for j = tau
     if(result_vec(i) == 1)
         Prob_matrix(i,j) = Prob_matrix(i,j)/ (1 - w);
