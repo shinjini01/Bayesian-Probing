1573733cc8b49e7da8a72d2ff74802f9bb525b90
shinjini01 <tuf56938@temple.edu>
2017-06-14 09:29:40 -0500

    Update update_prob.m

--- update_prob.m
+++ update_prob.m
@@ -1,34 +1,48 @@
-% To initialize the matrix with each column as a probe vector
-a1 = [ones(4,1); zeros(4,1)];
-a2 = repmat([1;1;0;0],2,1);
-a3 = repmat([1;0],4,1);
-t = [a1, 1-a1, a2, 1 - a2, a3, 1 - a3]; % t is the required matrix of probes
-a0 = repmat([1;0],3,1); 
-a4 = [a0; 1;1]; 
-a5 = [1-a0; 1;1];
-r.mat = [a1, 1 - a1, a2, 1 - a2, a4, a5]; % r.mat is the result matrix
+% P = ones(8,8)/8 ;    % Initialize the probability matrix
+% Q = 1 - P ;          % 1 - Probability matrix, required to calculate w values
+% % To initialize the matrix with each column as a probe vector
+% a1 = [ones(4,1); zeros(4,1)];
+% a2 = repmat([1;1;0;0],2,1);
+% a3 = repmat([1;0],4,1);
+% t = [a1, 1-a1, a2, 1 - a2, a3, 1 - a3]; % t is the required matrix of probes
+% a0 = repmat([1;0],3,1); 
+% a4 = [a0; 1;1]; 
+% a5 = [1-a0; 1;1];
+% r.mat = [a1, 1 - a1, a2, 1 - a2, a4, a5]; % r.mat is the result matrix
+% 
+% 
+% 
+% for k = 1:size(t,2)    
+% P = update_probability(t(:,k), P, r.mat(:,k))  
+%     
+% end
+%     
+%%%% Generalize the Algorithm %%%%
+
+%    Define the sparsity matrix
+
+S = diag(ones(4,1));    % Diagonal structure
+S(:,4) = 1; S(4,:) = 1; % Arrow type structure
+ 
+%%% Sparsity matrix from the suitesparse.com website
+%   Weblink: http://yifanhu.net/GALLERY/GRAPHS/search.html
+
+load cage4.mat
+sparsemat = full(Problem.A); 
+S = abs(sign(sparsemat)); % The sparse matrix
+
+[m_s, n_s] = size(S);     % Size of the sparse matrix
+S_prime = S;
+% Define the probability matrix
+P = ones(m_s,n_s)/min(m_s,n_s); % We begin with P matrix with identical elements 1/min(m, n)
+P_prime = zeros(m_s,n_s); 
+n_itr =0;                 % Counter for the number of interations
 
 
-
-for k = 1:size(t,2)    
-P = update_probability(t(:,k), P, r.mat(:,k))  
-    
-end
-    
-%% Generalize the Algorithm %%
-
- % We begin with P matrix with identical elements 1/min(m, n) 
-
-  S = diag(ones(4,1)); S(:,4) = 1; S(4,:) = 1; 
- load cage4.mat
- mat = full(Problem.A); S = abs(sign(mat));
- [m_s, n_s] = size(S); 
- S_prime = S;
- P = ones(m_s,n_s)/min(m_s,n_s) ; P_prime = zeros(m_s,n_s); n_itr =0;
  while( sum(sum(0<P<1)) ~=sum(sum(0<S_prime<1)))
      [m,n] = size(P); 
      t = zeros(n,1);
-     t(single_probe(P,m,n)) = 1;
+     t(single_probe3(P,m,n)) = 1;
      r = zeros(m,1); r(find(S_prime * t)) = 1;
      P = update_probability(t, P, r);
      indices = NaN(1,n);
@@ -43,8 +57,12 @@ end
          end
      end
      P_prime
+     
+     % Remove the fully identified columns from the probability matrix
+     % before the next iteration
      col_indices = (1:n).*(isnan(indices));
      P = P(:,col_indices(col_indices>0));
+     % Remove the corresponding columns from the sparsity matrix
      S_prime = S_prime(:,col_indices(col_indices>0));
      
      n_itr = n_itr+1;
