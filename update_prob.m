7696cb6fb44e25525b63f82bf2d9f4279dd1a2c5
shinjini01 <tuf56938@temple.edu>
2017-06-14 11:07:12 -0500

    Create update_prob.m

--- update_prob.m
+++ update_prob.m
@@ -1,22 +1,3 @@
-% P = ones(8,8)/8 ;    % Initialize the probability matrix
-% Q = 1 - P ;          % 1 - Probability matrix, required to calculate w values
-% % To initialize the matrix with each column as a probe vector
-% a1 = [ones(4,1); zeros(4,1)];
-% a2 = repmat([1;1;0;0],2,1);
-% a3 = repmat([1;0],4,1);
-% t = [a1, 1-a1, a2, 1 - a2, a3, 1 - a3]; % t is the required matrix of probes
-% a0 = repmat([1;0],3,1); 
-% a4 = [a0; 1;1]; 
-% a5 = [1-a0; 1;1];
-% r.mat = [a1, 1 - a1, a2, 1 - a2, a4, a5]; % r.mat is the result matrix
-% 
-% 
-% 
-% for k = 1:size(t,2)    
-% P = update_probability(t(:,k), P, r.mat(:,k))  
-%     
-% end
-%     
 %%%% Generalize the Algorithm %%%%
 
 %    Define the sparsity matrix
@@ -36,7 +17,7 @@ S_prime = S;
 % Define the probability matrix
 P = ones(m_s,n_s)/min(m_s,n_s); % We begin with P matrix with identical elements 1/min(m, n)
 P_prime = zeros(m_s,n_s); 
-n_itr =0;                 % Counter for the number of interations
+n_itr =0;                 % Counter for the number of iterations
 
 
  while( sum(sum(0<P<1)) ~=sum(sum(0<S_prime<1)))
@@ -65,6 +46,6 @@ n_itr =0;                 % Counter for the number of interations
      % Remove the corresponding columns from the sparsity matrix
      S_prime = S_prime(:,col_indices(col_indices>0));
      
-     n_itr = n_itr+1;
+     n_itr = n_itr+1
  end
  
