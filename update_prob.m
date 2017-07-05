6b237bfad8f55199afcd61772a0edcea43e1f681
shinjini01 <tuf56938@temple.edu>
2017-07-05 14:29:41 -0500

    Update update_prob.m

--- update_prob.m
+++ update_prob.m
@@ -54,11 +54,14 @@ n_itr =0;                 % Counter for the number of iterations
   %% Bundle Probing %%
  
 
+S = diag(ones(160,1));    % Diagonal structure
+S(:,160) = 1; S(160,:) = 1;
 S_prime = S;
 [m_s, n_s] = size(S);     % Size of the sparse matrix
-K = 6; batch_size = n_s/K;
-P = diag(sum(S,2)/n_s)*ones(m_s, n_s); P_prime2 = zeros(m_s, n_s);
-
+K = 32; batch_size = round(n_s/K);
+P = diag(sum(S,2)/n_s)*ones(m_s, n_s); 
+P_prime2 = zeros(m_s, n_s);
+%P = ones(m_s, n_s)/20;
 n_itr2 = 0;
 
 while( sum(and(0 < P, P <1)) ~= sum(and(0<S_prime, S_prime<1)))
@@ -72,13 +75,13 @@ for i = 1:K
     [m,n] = size(P_prime);  
     tau = single_probe3(P_prime,m,n);
     probe_mat(tau(1:min(size(tau,2),batch_size)),i) = 1;
-    P_prime(:,tau(1:min(size(tau,2),batch_size)))=[];
+    P_prime(:,tau(1:min(size(tau,2),batch_size)))= ones(m,min(size(tau,2),batch_size)) ;
 end
 % The corresponding result matrix
 r_mat(find(S_prime * probe_mat)) = 1;
-for i = 1:K
-    P = update_probability(probe_mat(:,i), P, r_mat(:,i));
-end
+
+    P = update_probability(probe_mat, P, r_mat);
+
 for i = 1:n_p
          for j = 1:n_s
              if(sum(abs(P(:,i)-S(:,j))< 0.0000000000001) == m_p)
@@ -98,3 +101,4 @@ for i = 1:n_p
      
      n_itr2= n_itr2+1;
  end
+ 
