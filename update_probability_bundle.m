14e2791cb8355bf91648fd0651881fcc209780b1
shinjini01 <tuf56938@temple.edu>
2017-07-20 17:20:12 -0500

    Add files via upload

--- update_probability_bundle.m
+++ update_probability_bundle.m
@@ -5,15 +5,15 @@
 %  Order: m x K
 %  Prob_matrix is the matrix of probabilities
 
-function P_mat = update_probability_bundle(probe_mat, Prob_matrix, result_mat)
-Q = 1 - Prob_matrix; tau = cell(1, size(probe_mat,2));
+function P_mat = update_probability_bundle(probe_mat, P, r_mat)
+Q = 1 - P; tau = cell(1, size(probe_mat,2));
 for i = 1:size(probe_mat,2)
     tau{i} = find(probe_mat(:,i)); % Find the indices of the non-zero values in the probe vectors
 end
 
 
-for i = 1: size(Prob_matrix, 1) %Taking one row of probability matrix
-    result_vec = result_mat(i,:);
+for i = 1: (size(P, 1)) %Taking one row of probability matrix
+    result_vec = r_mat(i,:);
     for j = 1:size(probe_mat,2)
         w(j) = prod(Q(i,tau{j})); % Calculate w_i values as in eq 12, section 2.5
         
@@ -24,18 +24,19 @@ for i = 1: size(Prob_matrix, 1) %Taking one row of probability matrix
         % Update the probability values as in eq. 15, section 2.5
         for k = tau{j}
             if(result_vec(j) == 1)
-               if(Prob_matrix(i,k)<1)
-                    Prob_matrix(i,k) = Prob_matrix(i,k)/ (1 - w(j));
+               if(P(i,k)>1)
+                   P(i,k) = P(i,k)/P(i,k);
+                  
                 else 
-                 Prob_matrix(i,k) = 1;
+                   P(i,k) = P(i,k)/ (1 - w(j));
                end
             else
-                Prob_matrix(i,k) = 0;
+                P(i,k) = 0;
             end
             
         end
     end
 end
 
-P_mat = Prob_matrix;
+P_mat = P;
 end
