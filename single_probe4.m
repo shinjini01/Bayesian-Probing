34549e62c84c968fdda72e46a870243892340662
shinjini01 <tuf56938@temple.edu>
2017-08-16 19:28:05 -0400

    Update single_probe4.m

--- single_probe4.m
+++ single_probe4.m
@@ -1,9 +1,6 @@
 %% Function to find single best probe as in section 3.3
-function tau = single_probe4(P, n)
+function tau = single_probe4(P)
 Q = 1 - P ;
-
-
-
 sum_indices = sum( (Q<1) .* Q, 1);
 delta_U = max(sum_indices);  %delta_U0 = delta_U;% delta_U(P,tau) as defined in eq. 14, section 2.5
 tau = find(sum_indices == delta_U,1);
@@ -11,37 +8,29 @@ tau = find(sum_indices == delta_U,1);
 indices = setdiff(I,tau); % Indices of the the columns not included in tau
 
 while(~isempty(indices))
-       
+    
     indices = setdiff(I,tau);
     %matrix of column index combinations
     delta_U_k = 0; indices_i0 = 0;
     l_tau = sum(Q(:,tau)<1, 2); w_tau = prod(Q(:,tau),2);
-   
+    
     for k = 1:length(indices)
         
         % recalculating delta_U for additional columns
-        
-        %delta_U_i_k = sum( (sum(Q(:,tau)<1, 2) + sum(Q(:,indices(k)) <1,2)) .* ( prod(Q(:,tau),2) .* prod(Q(:,indices(k)),2) ));
-     
         delta_U_i_k = sum( (l_tau + sum(Q(:,indices(k)) <1,2)) .* ( w_tau .* prod(Q(:,indices(k)),2) ));
         
         if(delta_U_i_k > delta_U_k)
-            delta_U_k = delta_U_i_k ; %Maximum delta value from a
+            delta_U_k = delta_U_i_k ; %Maximum delta value from adding a column to a probe
             indices_i0 = unique(sort(horzcat(tau, indices(k))));
-       
-        
-            end
+        end
     end
     
     if(delta_U_k > delta_U)
-        delta_U = delta_U_k
-        tau = indices_i0;
+        delta_U = delta_U_k;
+        tau = indices_i0; % determining the support of the probe vector
     else
-            break
-    end
-            
-     
+        break
+    end    
 end
 
 end
-
